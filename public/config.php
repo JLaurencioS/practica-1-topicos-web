4ab9c7b022135f71120c4e2b50bfe8370513968b
JLaurencioS <jlsr788@gmail.com>
2026-08-26 01:12:48 -0600

    minor changes

--- public/config.php
+++ public/config.php
@@ -1,10 +1,10 @@
 <?php
 // config.php - Conexión a la base de datos usando PDO (permite Prepared Statements)
 
-$host = 'db'; // o 'db' si usas el docker-compose que armamos
-$dbname = 'miapp_db';
-$user = 'miapp_user';
-$password = 'ClaveSegura123!'; // usa tu contraseña real
+$host = 'localhost'; 
+$dbname = 'bd_22030236'; // nombre de tu base de datos
+$user = 'u22030236';
+$password = '22030236'; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
